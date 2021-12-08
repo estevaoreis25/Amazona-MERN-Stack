d2e76c08eeed7ed9d62ae0b27e8281cf6b8f1b04
estevaoreis25 <estevaoreis1587@gmail.com>
2021-12-08 12:04:06 -0300

    Put all cards on a list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,139 +1,44 @@
-
+import data from './data'
 function App() {
   return (
     <div className="grid-container">
       <header className="row">
       <div>
-        <a className="brand" href="index.html">amazona</a>
+        <a className="brand" href="/">amazona</a>
       </div>
         <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
         </div>
         <div></div>
       </header>
      <main> 
       <div className="row center">
-        <div className="card">
-          <a href="product.html">
-            <img className="medium" src="./images/p1.jpg" alt="product"/>
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Rodof Slim Bob</h2>
+        {data.products.map(product =>(
+          <div className="card" key = {product._id}>
+            <a href={`product/${product._id}`}>
+              <img className="medium" 
+                  src={product.image} 
+                  alt={product.name}/>
             </a>
-            <div className="rating">
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-            </div>
-            <div className="price">
-              $120
+            <div className="card-body">
+              <a href={`product/${product._id}`}>
+                <h2>{product.name}</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">
+                {product.price}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="card">
-          <a href="product.html">
-            <img className="medium" src="./images/p1.jpg" alt="product"/>
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Rodof Slim Bob</h2>
-            </a>
-            <div className="rating">
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-            </div>
-            <div className="price">
-              $120
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <a href="product.html">
-            <img className="medium" src="./images/p1.jpg" alt="product"/>
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Rodof Slim Bob</h2>
-            </a>
-            <div className="rating">
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-            </div>
-            <div className="price">
-              $120
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <a href="product.html">
-            <img className="medium" src="./images/p1.jpg" alt="product"/>
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Rodof Slim Bob</h2>
-            </a>
-            <div className="rating">
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-            </div>
-            <div className="price">
-              $120
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <a href="product.html">
-            <img className="medium" src="./images/p1.jpg" alt="product"/>
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Rodof Slim Bob</h2>
-            </a>
-            <div className="rating">
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-            </div>
-            <div className="price">
-              $120
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <a href="product.html">
-            <img className="medium" src="./images/p1.jpg" alt="product"/>
-          </a>
-          <div className="card-body">
-            <a href="product.html">
-              <h2>Rodof Slim Bob</h2>
-            </a>
-            <div className="rating">
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star"></i></span>
-              <span><i className="fa fa-star-half-o"></i></span>
-            </div>
-            <div className="price">
-              $120
-            </div>
-          </div>
-        </div>
+         ))
+        }
       </div>
 
     </main>
