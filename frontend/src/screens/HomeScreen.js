df6a75bcf2d8e404417ef00663e691bd3c052c08
estevaoreis25 <estevaoreis1587@gmail.com>
2021-12-27 15:53:13 -0300

    Connecting backend to frontend and using api

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import data from '../data';
 import Product from '../components/Product';
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fecthDate();
+  }, []);
+
+  async function fecthDate() {
+    const { data } = await axios.get('/api/products');
+    setProducts(data);
+  };
+
   return (
     <div>
       <div className='row center'>
-        {data.products.map((product) => (
+        {products.map((product) => (
           <Product key={product._id} product={product} />
         ))}
       </div>
