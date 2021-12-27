e280642611e4bf9ffe82a4750bca7e62e759bef6
estevaoreis25 <estevaoreis1587@gmail.com>
2021-12-27 16:36:59 -0300

    Creation of Loading and Message component, styling them and using in HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,26 +2,39 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import data from '../data';
 import Product from '../components/Product';
+import MessageBox from '../components/MessageBox';
+import LoadingBox from '../components/LoadingBox';
 
 export default function HomeScreen() {
   const [products, setProducts] = useState([]);
-
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   useEffect(() => {
     fecthDate();
   }, []);
 
   async function fecthDate() {
-    const { data } = await axios.get('/api/products');
-    setProducts(data);
+    try {
+      setLoading(true);
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    }
   };
 
   return (
     <div>
-      <div className='row center'>
-        {products.map((product) => (
-          <Product key={product._id} product={product} />
-        ))}
-      </div>
-    </div>
+      {loading ? <LoadingBox /> :
+        error ? <MessageBox variant="danger">{error}</MessageBox> :
+          (<div className='row center'>
+            {products.map((product) => (
+              <Product key={product._id} product={product} />
+            ))}
+          </div>)
+      }
+    </div >
   )
 }
