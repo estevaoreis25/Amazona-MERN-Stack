66d95bac022ac15ca4c89adbd2725ebfc0dc6d43
estevaoreis25 <estevaoreis1587@gmail.com>
2022-01-09 11:04:55 -0300

    Creation of the icon that shows the quantity of items selected in the cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,11 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import CartScreen from './screens/CartScreen';
+import { useSelector } from 'react-redux';
 function App() {
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
       <div className='grid-container'>
@@ -14,7 +18,11 @@ function App() {
             </Link>
           </div>
           <div>
-            <Link to='/cart'>Cart</Link>
+            <Link to='/cart'>Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
             <Link to='/signin'>Sign In</Link>
           </div>
         </header>
